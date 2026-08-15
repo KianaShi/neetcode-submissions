302f754a95d6770f38b2200845a51011f1a0e379
KianaShi <shi.jiaqi1@northeastern.edu>
2026-08-15 14:57:30 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        countS = {}
+        countT = {}
+        for c in s:
+            if c in countS:
+                countS[c] += 1
+            else:
+                countS[c] = 1
+        
+        for q in t:
+            if q in countS:
+                countS[q] -= 1
+            else:
+                countS[q] = 1
+        
+        for value in countS.values():
+            if value != 0:
+                return False
+        
+        return True
+        
